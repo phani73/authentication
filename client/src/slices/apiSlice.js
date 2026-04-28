549a302190c738d135d5a84963a1c849cc42586e
phani73 <phanivarma181@gmail.com>
2026-04-28 23:02:58 +0530

    authentication changes

--- client/src/slices/apiSlice.js
+++ client/src/slices/apiSlice.js
@@ -1,8 +1,9 @@
 import { fetchBaseQuery, createApi } from '@reduxjs/toolkit/query/react';
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: import.meta.env.VITE_API_URL || 'http://localhost:5000',
-  credentials: 'include'
+  baseUrl:
+    import.meta.env.VITE_API_URL || "https://authentication.onrender.com",
+  credentials: "include",
 });
 
 export const apiSlice = createApi({
